b73167ffd8613ba3e81ec4b33883c283415d1ffd
z-chu <zchu8073@gmail.com>
2017-07-22 14:36:06 +0800

    Upgrade glide version to 4.0.0-RC1

---
+++ app/src/main/java/com/youshibi/app/util/GlobalGlideModule.java
@@ -0,0 +1,11 @@
+package com.youshibi.app.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Chu on 2017/7/22.
+ */
+@GlideModule
+public class GlobalGlideModule extends AppGlideModule{
+}
