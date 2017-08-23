4cb665a7fe21ff129620d5667cb97ef7d1c7a675
zchu <zhaocz@pifubao.com>
2017-08-23 17:20:51 +0800

    Add mine feature code

---
+++ app/src/main/java/com/youshibi/app/ui/widget/MineRowView.java
@@ -0,0 +1,26 @@
+package com.youshibi.app.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * author : zchu
+ * date   : 2017/8/23
+ * desc   :
+ */
+
+public class MineRowView extends RelativeLayout {
+
+    public MineRowView(Context context) {
+        super(context);
+    }
+
+    public MineRowView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MineRowView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
