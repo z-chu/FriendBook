8b7ad06ecc3349045f90f11a9db2996088994e58
z-chu <zchu8073@gmail.com>
2017-09-12 21:29:49 +0800

    Clear old File

--- app/src/main/java/com/youshibi/app/presentation/StartActivity.java
+++ app/src/main/java/com/youshibi/app/presentation/StartActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.youshibi.app.AppRouter;
-import com.youshibi.app.BuildConfig;
 import com.youshibi.app.base.BaseActivity;
 import com.youshibi.app.data.prefs.PreferencesHelper;
 import com.youshibi.app.util.FileUtil;
@@ -22,10 +21,8 @@ public class StartActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         // TODO: 16-11-23 一些UI逻辑、检查缓存版本（更新后缓存是否还可用）
         if (PreferencesHelper.getInstance().isFirstStartApp()) {
-            if (BuildConfig.VERSION_CODE == 2) {
-                FileUtil.clearFileWithPath(getFilesDir().getPath());
-                FileUtil.clearFileWithPath(getCacheDir().getPath());
-            }
+            FileUtil.clearFileWithPath(getFilesDir().getPath());
+            FileUtil.clearFileWithPath(getCacheDir().getPath());
             PreferencesHelper.getInstance().setFirstStartApp(false);
         }
         if (isLogin()) {
